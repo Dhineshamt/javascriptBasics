88c07c440afa192e1247168a36d7d0f022c92a97
Dhineshamt <a.dhinesh1@gmail.com>
2020-03-28 03:30:19 +0530

    IIFE function update

--- src/IIFEfunction.js
+++ src/IIFEfunction.js
@@ -5,9 +5,10 @@
 /* Output: Simple IIFE function */
 
 /* IIFE function with parameters */
-((date) => {
-    console.log(date);
-}) (new Date());
+((...data) => { //Used rest parameter
+    let [date, name] = data; //Used array destructure
+    console.log(date, name);
+}) (new Date(), 'Dhinesh');  //Used rest parameter
 /* Output: 2020-03-27T20:49:56.214Z*/
 
 /* IIFE function that returns a value */
